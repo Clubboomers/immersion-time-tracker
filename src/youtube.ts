01b1fae7bfb58e520eacb7efdbc3cac87aa357da
Clubboomers <tim@hogberg.net>
2023-05-16 14:21:07 +0200

    Added support for netflix
    The extension will now track time whenever you play something on netflix

--- src/youtube.ts
+++ src/youtube.ts
@@ -7,10 +7,59 @@ let lastUrl: string = thisUrl;
 let videoIsPlaying: boolean | undefined = false;
 let videoHasListeners: boolean = false;
 let hasPlayed: boolean = false;
+const validVideoUrls: string[] = ["youtube.com/watch", "netflix.com/watch"];
+const invalidVideoUrls: string[] = ["music.youtube.com"];
 
 console.log("Content script loaded");
-if (isYoutubeVideo(thisUrl)) {
+if (isVideoUrl(thisUrl)) {
   initVideo();
+}
+
+async function initVideo() {
+  hasPlayed = false;
+  videoHasListeners = false;
+  videoElement = null;
+  videoTitle = null;
+  videoIsPlaying = undefined;
+  videoElement = await getVideoElement();
+  if (thisUrl.includes("youtube.com/watch")) {
+    videoTitle = await getVideoTitleYoutube().then((title) => {
+      if (title) {
+        console.log("title: " + title);
+        return title;
+      } else {
+        return "";
+      }
+    });
+  } else if (thisUrl.includes("netflix.com/watch")) {
+    videoTitle = await getVideoTitleNetflix().then((title) => {
+      if (title) {
+        console.log("title: " + title);
+        return title;
+      } else {
+        return "";
+      }
+    });
+  }
+  if (videoElement) {
+    hasPlayed = videoIsPlaying = isPlaying();
+    await sendUpdateToBackground(videoTitle, thisUrl, isPlaying());
+    addEventListeners(videoElement);
+  }
+  console.log(!!videoElement);
+}
+
+function updatedUrl(): void {
+  if (videoIsPlaying) {
+    // if video was still playing before url changed
+    console.log("video is playing");
+    videoIsPlaying = false;
+    sendUpdateToBackground(videoTitle, lastUrl, videoIsPlaying);
+  }
+  console.log("url has changed");
+  if (isVideoUrl(thisUrl)) {
+    initVideo();
+  }
 }
 
 /**
@@ -25,43 +74,13 @@ let intervalId = setInterval(() => {
   }
 }, 1000);
 
-function updatedUrl(): void {
-    if (videoIsPlaying) { // if video was still playing before url changed
-        console.log("video is playing");
-        videoIsPlaying = false;
-        sendUpdateToBackground(videoTitle, lastUrl, videoIsPlaying);
-    }
-  console.log("url has changed");
-  if (isYoutubeVideo(thisUrl)) {
-    initVideo();
-  }
-}
-
-async function initVideo() {
-  hasPlayed = false;
-  videoHasListeners = false;
-  videoElement = null;
-  videoIsPlaying = undefined;
-  videoElement = await getVideoElement();
-  videoTitle = await getVideoTitle().then((title) => {
-    if (title) {
-      console.log("title: " + title);
-      return title;
-    } else {
-      return "";
-    }
-  });
-  if (videoElement) {
-    hasPlayed = videoIsPlaying = isPlaying();
-    await sendUpdateToBackground(videoTitle, thisUrl, isPlaying());
-    addEventListeners(videoElement);
-  }
-  console.log(!!videoElement);
-}
-
-function isYoutubeVideo(url: string | undefined): boolean {
+function isVideoUrl(url: string | undefined): boolean {
   if (!url) return false;
-  if (url.includes("youtube.com/watch") && !url.includes("music.youtube")) {
+  const validVideoUrl: boolean =
+    validVideoUrls.find((str) => url.includes(str)) !== undefined;
+  const invalidVideoUrl: boolean =
+    invalidVideoUrls.find((str) => url.includes(str)) !== undefined;
+  if (validVideoUrl && !invalidVideoUrl) {
     return true;
   }
   return false;
@@ -87,7 +106,11 @@ function addEventListeners(videoElement: HTMLVideoElement): void {
   }
 }
 
-function sendUpdateToBackground(videoTitle: string | null, url: string, isPlaying: boolean): void {
+function sendUpdateToBackground(
+  videoTitle: string | null,
+  url: string,
+  isPlaying: boolean
+): void {
   if (!hasPlayed) return; // don't send update if video hasn't played yet
   console.log("sending update to background");
   chrome.runtime.sendMessage({
@@ -130,7 +153,7 @@ async function getVideoElement(): Promise<HTMLVideoElement | null> {
   });
 }
 
-async function getVideoTitle(): Promise<string | null> {
+async function getVideoTitleYoutube(): Promise<string | null> {
   // select the title element
   let titleElement = document.querySelector(
     "#container > h1 > yt-formatted-string"
@@ -151,10 +174,34 @@ async function getVideoTitle(): Promise<string | null> {
         resolve(titleElement.innerHTML);
       }
       totalTimeout += 100;
-        if (totalTimeout > 5000) {
-          clearInterval(intervalId);
-          resolve(null);
-        }
+      if (totalTimeout > 5000) {
+        clearInterval(intervalId);
+        resolve(null);
+      }
+    }, 100);
+  });
+}
+
+async function getVideoTitleNetflix(): Promise<string | null> {
+  let titleElement = document.querySelector("div[data-title]");
+
+  if (titleElement) {
+    return titleElement.getAttribute("data-title");
+  }
+
+  return new Promise((resolve, reject) => {
+    let totalTimeout = 0;
+    intervalId = setInterval(() => {
+      titleElement = document.querySelector("div[data-title]");
+      if (titleElement) {
+        clearInterval(intervalId);
+        resolve(titleElement.getAttribute("data-title"));
+      }
+      totalTimeout += 100;
+      if (totalTimeout > 5000) {
+        clearInterval(intervalId);
+        resolve(null);
+      }
     }, 100);
   });
 }
